5e8386976e71620f8e92958a33a5cd3fe45c1043
chie-akiba <68101446+chie-akiba@users.noreply.github.com>
2020-08-09 18:55:08 +0900

    ファイルの追加（初コミット）

---
+++ MapApp/MapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by ITG3-00935 on 2020/08/08.
+//  Copyright © 2020 ITG3-00935. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
